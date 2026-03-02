d6fdf803eee8069ef9dfd6eaa81e39e7e94145ca
Aaron D'Mello <aaron.r.dmello@gmail.com>
2026-03-02 12:19:57 +1000

    fixed methods on health endpoints

--- todo/views/routes.py
+++ todo/views/routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify
 
 api = Blueprint('api', __name__, url_prefix='/api/v1')
 
-@api.route('/health', methods=['GET'])
+@api.route('/health')
 def health():
     return jsonify({'status': 'ok'})
 
