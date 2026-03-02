d4073884d24c0f2b72a646d03b601f70bff56a0a
Aaron D'Mello <aaron.r.dmello@gmail.com>
2026-03-02 12:50:51 +1000

    idk why this not working

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,9 +1,9 @@
-from flask import Flask, app
+from flask import Flask
 
 def create_app():
     app = Flask(__name__)
 
     from .views.routes import api
     app.register_blueprint(api)
-    
+
     return app
